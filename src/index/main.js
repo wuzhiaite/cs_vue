a9e48e011967542b5e497b89844a15ebee4b863a
wuzhiaite <656494298@qq.com>
2020-08-24 14:29:03 +0800

    调整代码

--- src/index/main.js
+++ src/index/main.js
@@ -1,19 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue';
+import store from  '../store/index';
+import router from './routers/ConfigRouter';
+import i18n from "../i18n/i18n";
+import '../plugin/ComBindPlugin';
+import '../plugin/index';//注册基础组件
+import '../util/axios';//axios拦截器处理
+import '../util/base/commonUtil';//全局通用方法
+import '../util/com-filter';//通用filter
 import './element-variables.scss';
 import '../style/theme/index.css';
 import '../util/el-components';
-import store from  '../store/index';
-import router from './routers/ConfigRouter';
-import '../plugin/ComBindPlugin';
-import '../plugin/index';
-import VueAxiosPlugin from 'vue-axios-plugin';
-import '../util/axios';
-import '../util/base/commonUtil';
 import 'video.js/dist/video-js.css'
-import i18n from "../i18n/i18n";
-import '../util/com-filter';//通用filter
-
 
 
 i18n.locale = store.state.lang || 'zh-cn';
